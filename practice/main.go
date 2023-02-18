5ca4b845a438378c56f4f82d181df7d16233da0c
Rudiath Rizky Adi Nugraha <rudiath.r@gmail.com>
2023-02-18 10:27:47 +0700

    f15 done

---
+++ practice/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"practice/controllers"
+	"practice/database"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+var (
+	DB  *sql.DB
+	err error
+)
+
+func main() {
+
+	//ENV Configuration
+	err = godotenv.Load("config/.env")
+	if err != nil {
+		fmt.Println("failed load file environment")
+	} else {
+		fmt.Println("success load file environment")
+	}
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"))
+
+	DB, err = sql.Open("postgres", psqlInfo)
+	err = DB.Ping()
+	if err != nil {
+		fmt.Println("DB Connection Failed")
+		panic(err)
+	} else {
+		fmt.Println("DB Connection Success")
+	}
+
+	database.DbMigrate(DB)
+
+	defer DB.Close()
+
+	//Router GIN
+	router := gin.Default()
+	router.GET("/persons", controllers.GetAllPerson)
+	router.POST("/persons", controllers.InsertPerson)
+	router.PUT("/persons/:id", controllers.UpdatedPerson)
+	router.DELETE("/persons/:id", controllers.DeletedPerson)
+
+	router.Run("localhost:8080")
+}
